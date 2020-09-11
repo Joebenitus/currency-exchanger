4b2a8e2cee6b9ccb760148d5122e7067fc3a7330
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-11 11:17:36 -0700

    refactor code to utilize 'codes.js' to output text correctly

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from "./js/conversions.js"
+import currencyCodes from "./js/codes.js"
 
 $(document).ready(function() {
   $("#btn-submit").click(function() {
-    let usd = $("#dollar-amt").val();
+    let usd = parseFloat($("#dollar-amt").val());
     let currency = $("#currency").val();
     let promise = Exchange.convert();
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $(".showConversion").text(`${usd} US dollars equals ${body.conversion_rates.currency}`);
+      $(".showConversion").text(`${usd} US dollars equals ${(parseFloat(body.conversion_rates[currency]) * usd)} ${currencyCodes[currency]}`);
     }, function(error) {
       $(".showError").text(`There was an error processing your request: ${error}`);
     })
