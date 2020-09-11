ebf0e6e50486eabc57518c44e8424c340539935f
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-11 14:48:10 -0700

    account for invalid user input

--- src/main.js
+++ src/main.js
@@ -10,14 +10,19 @@ $(document).ready(function() {
     let currency1 = $("#currency1").val().toUpperCase();
     let currency2 = $("#currency2").val().toUpperCase();
     let amount = parseFloat($("#amount").val());
-    let promise = Exchange.convert(currency1);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      let conversion = (parseFloat(body.conversion_rates[currency2]) * amount).toFixed(2)
-      $(".showConversion").text(`${amount} ${currencyCodes[currency1]} = ${conversion} ${currencyCodes[currency2]}`);
-    }, function(error) {
-      $(".showError").text(`There was an error processing your request: ${error}`);
-    })
+    if ((currency1 && currency2 && amount) && currency1 in currencyCodes && currency2 in currencyCodes) {
+      let promise = Exchange.convert(currency1);
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        let conversion = (parseFloat(body.conversion_rates[currency2]) * amount).toFixed(2)
+        $(".showConversion").text(`${amount} ${currencyCodes[currency1]} = ${conversion} ${currencyCodes[currency2]}`);
+      }, function(error) {
+        $(".showError").text(`There was an error processing your request: ${error}`);
+      })
+    } else {
+      $(".showConversion").text("Invalid input(s)");
+    }
+    
   });
 });
 
