955783db773b8fe8af3c95663ba4450ca8a4a558
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-11 10:24:36 -0700

    add list of accepted conversions and add field to enter 3 letter currency code

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import Exchange from "./js/conversions.js"
 $(document).ready(function() {
   $("#btn-submit").click(function() {
     let usd = $("#dollar-amt").val();
+    let currency = $("#currency").val();
     let promise = Exchange.convert();
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $(".showConversion").text(`${usd} US dollars equals ${body.conversion_rates.MXN}`);
+      $(".showConversion").text(`${usd} US dollars equals ${body.conversion_rates.currency}`);
     }, function(error) {
       $(".showError").text(`There was an error processing your request: ${error}`);
     })
