d2484ee8a8e30b86ed77f2b1418cb745bd336794
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-11 11:15:52 -0700

    add js file which contains the definitions of the available currency codes

---
+++ src/js/codes.js
@@ -0,0 +1,10 @@
+const currencyCodes = {
+  "MXN": "Mexican Pesos.",
+  "AUD": "Australian Dollars.",
+  "CAD": "Canadian Dollars.",
+  "EUR": "Euro.",
+  "INR": "Indian Rupees.",
+  "JPY": "Japanese Yen."
+}
+
+export default currencyCodes
